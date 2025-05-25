65fe434b56895775c7809ec778072141e3862ff1
MikailWho <mwhangyee@gmail.com>
2025-05-25 10:05:04 +0700

    Edit register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,10 +12,6 @@
         <form action="{{ route('register') }}" method="POST">
             @csrf
             <div class="form-group">
-                <label for="name">Full Name</label>
-                <input type="text" id="name" name="name" placeholder="Enter your full name" required>
-            </div>
-            <div class="form-group">
                 <label for="email">Email Address</label>
                 <input type="email" id="email" name="email" placeholder="Enter your email" required>
             </div>
