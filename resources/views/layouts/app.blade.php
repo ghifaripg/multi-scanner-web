f8d048f65017c40dbeb0cfd2fe70f4fc58049328
MikailWho <mwhangyee@gmail.com>
2025-05-26 13:04:27 +0700

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>ThreatPeek</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/Logo.png') }}">
     <style>
-    body {
-        background-color: #ffffff;
-        margin: 0;
-        padding: 0;
-    }
-</style>
+        body {
+            background-color: #ffffff;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
 </head>
+
 <body>
 
     @include('partials.navbar')
@@ -31,4 +34,5 @@
 
 
 </body>
+
 </html>
